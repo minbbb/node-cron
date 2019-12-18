9b56db224905d79663db3e08585b97e0599cee97
minbbb <maks4339@gmail.com>
2019-12-18 22:40:09 +0300

    Added new functionality for one-time operation

--- cron.js
+++ cron.js
@@ -2,7 +2,7 @@ class Cron{
 	constructor(time, func){
 		this.reTime = /(.+)\s+(.+)\s+(.+)\s+(.+)\s+(.+)/;
 		if(!this.reTime.test(time))
-			throw "wrong pattern time";
+			throw "wrong time pattern";
 		this.time = time;
 		this.func = func;
 	}
@@ -31,9 +31,36 @@ class Cron{
 			this.start();
 		}, toTime);
 	}
+	once(){
+		var correctTime = function(execReTime, calcTime){
+			if((execReTime[5] == "*" || calcTime.getMonth() == parseInt(execReTime[5], 10)) && (execReTime[4] == "*" || calcTime.getDate() == parseInt(execReTime[4], 10)) && (execReTime[3] == "*" || calcTime.getHours() == parseInt(execReTime[3], 10)) && (execReTime[2] == "*" || calcTime.getMinutes() == parseInt(execReTime[2], 10)) && (execReTime[1] == "*" || calcTime.getSeconds() == parseInt(execReTime[1], 10))){
+				return true;
+			}
+			return false;
+		}
+		var now = new Date();
+		var calcTime = new Date();
+		var execReTime = this.reTime.exec(this.time);
+		calcTime.setMilliseconds(0);
+		var toTime = 0;
+		while(toTime <= 0){
+			while(true){
+				calcTime.setSeconds(calcTime.getSeconds() + 1);
+				if(correctTime(execReTime, calcTime))
+					break;
+			}
+			toTime = calcTime - now;
+		}
+		this.timerIdOnce = setTimeout(() => {
+			this.func();
+		}, toTime);
+	}
 	stop(){
 		if(this.timerId != undefined){
 			clearTimeout(this.timerId);
+		}
+		if(this.timerIdOnce != undefined){
+			clearTimeout(this.timerIdOnce);
 		}
 	}
 }
